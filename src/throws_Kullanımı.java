50b48a285c14f72649ea3c09ba75672dfc634cd2
msevtekin <msevtekin@gmail.com>
2024-11-22 15:40:10 +0300

    ilk commit

---
+++ src/throws_Kullanımı.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+public class throws_Kullanımı {
+
+    public static void yasKontrol(int yas) throws IOException {
+        if(yas < 18) {
+            throw new IOException("Yasınız 18'den kuçuk.");
+        }else {
+            System.out.println("Yasal yaştasınız.");
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        yasKontrol(10);
+    }
+}
